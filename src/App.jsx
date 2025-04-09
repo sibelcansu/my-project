0d580f7325ee34eeca4297c426511d9e00f359cb
sibelcansu <sibelkcansu@gmail.com>
2025-04-09 14:06:53 +0300

    useState hook eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,82 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [ad, setAd] = useState("");
+  const [kartNo, setKartNo] = useState("");
+  const [guvenlik, setGuvenlik] = useState("");
+  
+  const handleClick = () => {
+    alert(`
+      Adınız: ${ad}
+      Kart Numaranız: ${kartNo}
+      Güvenlik Kodunuz: ${guvenlik}`);
+  };
+
   return (
-    <div className='paymentContainer'>
-      <h2 className='title'>Ödeme Bilgileri</h2>
+    <div className="paymentContainer">
+      <h2 className="title">Ödeme Bilgileri</h2>
       <p className="subtitle">Kredi kartı bilgilerini giriniz</p>
 
       <form className="paymentForm">
         <div className="formGroup">
           <label>Kart Üzerindeki İsim</label>
-          <input type="text" placeholder="Adınızı Giriniz"/>
+          <input
+            type="text"
+            placeholder="Adınızı Giriniz"
+            onChange={(e) => setAd(e.target.value)}
+          />
         </div>
 
         <div className="formGroup">
           <label>Kart Numarası</label>
-          <input type="text" placeholder="0000 0000 0000 0000"/>
+          <input
+            type="text"
+            placeholder="0000 0000 0000 0000"
+            onChange={(e) => setKartNo(e.target.value)}
+          />
         </div>
 
-        <div className="formGroup">
-          <label>Ay</label>
-          <input type="text" placeholder="Adınızı Giriniz"/>
+        <div className="selectContainer">
+          <div className="formGroup">
+            <label>Ay</label>
+            <select>
+              <option>AA</option>
+            </select>
+          </div>
+
+          <div className="formGroup">
+            <label>Yıl</label>
+            <select>
+              <option>AA</option>
+            </select>
+          </div>
+
+          <div className="formGroup">
+            <label>Güvenlik Kodu</label>
+            <input
+              type="text"
+              placeholder="123"
+              onChange={(e) => setGuvenlik(e.target.value)}
+            />
+          </div>
         </div>
 
-        <div className="formGroup">
-          <label>Yıl</label>
-          <input type="text" placeholder=""/>
-        </div>
+        <button className="btn" onClick={handleClick}>Gönder</button>
 
-        <div className="formGroup">
-          <label>Güvenlik Kodu</label>
-          <input type="text" placeholder="123"/>
-        </div>
-
-        <Button>Şimdi Öde</Button>
+        
       </form>
-      
     </div>
-  )
-}
-
-const Button=(props)=>{
-  return <button className="btn" type="submit">{props.children}</button>;
-
+  );
 };
 
-const Metin=(props)=>{
+
+const Metin = (props) => {
   return <h2>{props.children}</h2>;
-}
+};
 
-const Bilgiler=(props)=>{
-  return <h4>{props.children}</h4>
+const Bilgiler = (props) => {
+  return <h4>{props.children}</h4>;
+};
+export default App;
 
-}
-export default App
-
-{/* <Metin>Ödeme Bilgileri</Metin>
-      <p>Kredi kartı bilgilerinizi giriniz</p>
-      <Bilgiler>Kart Üzerindeki İsim</Bilgiler>
-      <input></input>
-      <Bilgiler>Kart Numarası</Bilgiler>
-
-       */}
